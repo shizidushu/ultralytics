d949e355aa7614258dffc34552a796b546263894
shizidushu <shizidushu@gmail.com>
2024-03-24 12:17:06 +0800

    start training

---
+++ train-pose.py
@@ -0,0 +1,25 @@
+from ultralytics import settings
+from ultralytics import YOLO
+import torch
+import os
+
+current_folder = os.path.dirname(__file__)
+datasets_dir = '/mnt/d/0-Datasets/ultralytics_datasets_dir/datasets'
+weights_dir = os.path.join(current_folder, "weights")
+runs_dir = os.path.join(current_folder, "runs")
+
+settings.update({'datasets_dir': datasets_dir})
+settings.update({'weights_dir': weights_dir})
+settings.update({'runs_dir': runs_dir})
+os.makedirs(weights_dir, exist_ok=True)
+os.makedirs(runs_dir, exist_ok=True)
+
+resume = False
+if not resume:
+    # Create a new YOLO model from scratch
+    model = YOLO('yolov8n-pose.yaml', task='pose').load('yolov8n-pose.pt')
+else:
+    model = YOLO('runs/pose/train/weights/last.pt', task='pose')
+
+device_count = torch.cuda.device_count()
+model.train(data='coco-pose.yaml', imgsz=640, epochs=200, resume=resume, batch = 32 * device_count, device=list(range(0,device_count)))
---
+++ training_note.md
@@ -0,0 +1,13 @@
+
+
+
+Strat training Pose on coco
+
+```bash
+python train-pose.py
+```
+
+Start training Detect on coco
+```bash
+python train.py
+```
